6adaae33f00e35ec49e643c98ef8000922f93b55
Ian Laird <ian_laird@baylor.edu>
2018-12-09 17:42:28 -0600

    added msiiing file

---
+++ takes_away.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>UnRegister for Class</title>
+
+    <?php include("components/header.php"); ?>
+    <?php include("database.php"); ?>
+	<?php include("user_validation.php") ?>
+
+
+</head>
+
+<body id="page-top">
+
+    <?php include("components/navbar.php"); ?>
+
+    <div id="wrapper">
+
+        <?php include("components/sidebar.php"); ?>
+
+        <div id="content-wrapper">
+
+            <div class="container-fluid">
+
+                <form method="post" action="remove_takes.php">
+
+                    <!-- Name -->
+                    <div class="form-group">
+                        <div class="form-row row">
+                            <div class="col-md-2">
+                                <h5>CRN number</h5>
+                            </div>
+                            <div class="col-md-2">
+                                <input type="text" name="crn" class="form-control" required="required">
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <input class='btn btn-primary btn-block col-md-3' type='submit' name='next' value='Next'>
+
+                </form>
+
+            </div>
+            <!-- /.container-fluid -->
+
+            <?php include("components/footer.php"); ?>
+
+        </div>
+        <!-- /.content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+</body>
+
+</html>
